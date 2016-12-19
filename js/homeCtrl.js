3a1d25da159fda075f14cc448b631c0a9fa7ad47
asemle <asemle@umass.edu>
2016-12-19 16:05:12 -0700

    background-attachment:fixed nto working on mobile, testing workaround

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -1,7 +1,7 @@
 angular.module('healing')
 .controller('homeCtrl', function($scope, $element, $document) {
-  $(window).bind('scroll', function() {
-     if ($(window).scrollTop() > 0) {
+  $('.background').bind('scroll', function() {
+     if ($('.background').scrollTop() > 0) {
          $('.glyphicon-chevron-left').fadeOut();
          $('.glyphicon-chevron-right').fadeOut();
      }
@@ -11,54 +11,54 @@ angular.module('healing')
      }
    });
    var perc = $('.home').height() * .01;
-   $(window).bind('scroll', function() {
-      if ($(window).scrollTop() > perc * 2) {
+   $('.background').bind('scroll', function() {
+      if ($('.background').scrollTop() > perc * 2) {
           $('.q1').fadeOut();
       }
       //q2
-      if ($(window).scrollTop() > perc * 5 && $(window).scrollTop() < perc * 14) {
+      if ($('.background').scrollTop() > perc * 5 && $('.background').scrollTop() < perc * 14) {
           $('.q2').fadeIn();
       }
       else {
           $('.q2').fadeOut();
       }
       //q3
-      if ($(window).scrollTop() > perc * 17 && $(window).scrollTop() < perc * 25) {
+      if ($('.background').scrollTop() > perc * 17 && $('.background').scrollTop() < perc * 25) {
           $('.q3').fadeIn();
       }
       else {
           $('.q3').fadeOut();
       }
       //q4
-      if ($(window).scrollTop() > perc * 28 && $(window).scrollTop() < perc * 37) {
+      if ($('.background').scrollTop() > perc * 28 && $('.background').scrollTop() < perc * 37) {
           $('.q4').fadeIn();
       }
       else {
           $('.q4').fadeOut();
       }
       //q5
-      if ($(window).scrollTop() > perc * 39 && $(window).scrollTop() < perc * 47) {
+      if ($('.background').scrollTop() > perc * 39 && $('.background').scrollTop() < perc * 47) {
           $('.q5').fadeIn();
       }
       else {
           $('.q5').fadeOut();
       }
       //q6
-      if ($(window).scrollTop() > perc * 50 && $(window).scrollTop() < perc * 58) {
+      if ($('.background').scrollTop() > perc * 50 && $('.background').scrollTop() < perc * 58) {
           $('.q6').fadeIn();
       }
       else {
           $('.q6').fadeOut();
       }
       //q7
-      if ($(window).scrollTop() > perc * 60 && $(window).scrollTop() < perc * 71) {
+      if ($('.background').scrollTop() > perc * 60 && $('.background').scrollTop() < perc * 71) {
             $('.q7').fadeIn();
       }
       else {
           $('.q7').fadeOut();
       }
         //q8
-      if ($(window).scrollTop() > perc * 73 && $(window).scrollTop() < perc * 85) {
+      if ($('.background').scrollTop() > perc * 73 && $('.background').scrollTop() < perc * 85) {
           $('.q8').fadeIn(1400);
       }
       else {
@@ -66,8 +66,8 @@ angular.module('healing')
       }
     });
 
-    //   var vh = $(window).height()/100;
-    // $(window).bind('scroll', function() {
+    //   var vh = $('.background').height()/100;
+    // $('.background').bind('scroll', function() {
     //   var element = $element.find('.firstQ');
     //   if(element.getBoundingClientRect().top < (48*vh)) {
     //     element.fadeOut()
